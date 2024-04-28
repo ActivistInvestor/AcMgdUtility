6583539751cb2af6d5b4ddfc96e7232c4f4cf9b9
Tony <caddzooks@gmail.com>
2024-04-28 03:56:05 -0400

    Update MyRibbonExtensionApplication.cs

--- MyRibbonExtensionApplication.cs
+++ MyRibbonExtensionApplication.cs
@@ -1,17 +1,32 @@
-﻿/// MyRibbonExtensionApplication
+﻿/// MyRibbonExtensionApplication Example class
 /// ActivistInvestor / Tony T
 /// 
-/// Example concrete implementation of the
-/// RibbonExtensionApplication, demonstrating
-/// how that class is used.
-///
+/// <summary>
+/// Using the RibbonExtensionApplication class is easy. You
+/// you do not have to modify that class, you only need to 
+/// derive a new class from it, like the example shown below, 
+/// and override/implement the InitializeRibbon() method, and    
+/// optionally the Initialize(), and/or Terminate() methods.
+/// 
+/// The example below can be used as a template for building 
+/// your own derived type.
+/// 
 /// Disclaimer: Use of RibbonExtensionApplication does not
 /// require modification of that class, it only requires 
-/// deriving a new class from it, and overriding one, two,
-/// or three virtual methods. If you instead use the base 
-/// class by modifying it, you are on your own, and please 
-/// do not ask for help or support, because you're not using 
-/// the code as it was indended to be used.
+/// deriving a new class from it, like the example shown
+/// below. 
+/// 
+/// If you instead use the base class by modifying it, you're
+/// on your own, and please do not ask for help or support, 
+/// because you're not using the code in the manner that it
+/// was indended to be used.
+/// 
+/// You can post comments, issues, flames or whatever in the
+/// repo discussion at:
+/// 
+///    https://github.com/ActivistInvestor/AcMgdUtility/discussions
+///    
+/// </summary>
 
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.Runtime.AIUtils;
