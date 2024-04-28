b225b153b0a8d2378da12985feff07630626dc5c
Tony <caddzooks@gmail.com>
2024-04-28 03:43:32 -0400

    Update MyRibbonExtensionApplication.cs

--- MyRibbonExtensionApplication.cs
+++ MyRibbonExtensionApplication.cs
@@ -4,6 +4,14 @@
 /// Example concrete implementation of the
 /// RibbonExtensionApplication, demonstrating
 /// how that class is used.
+///
+/// Disclaimer: Use of RibbonExtensionApplication does not
+/// require modification of that class, it only requires 
+/// deriving a new class from it, and overriding one, two,
+/// or three virtual methods. If you instead use the base 
+/// class by modifying it, you are on your own, and please 
+/// do not ask for help or support, because you're not using 
+/// the code as it was indended to be used.
 
 using Autodesk.AutoCAD.Runtime;
 using Autodesk.AutoCAD.Runtime.AIUtils;
