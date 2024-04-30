c4106798e196381a4f6e3ff8c650282715bdcdeb
Tony <caddzooks@gmail.com>
2024-04-30 01:35:07 -0400

    Add files via upload

--- RibbonExtensionApplication.cs
+++ RibbonExtensionApplication.cs
@@ -10,6 +10,11 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.Ribbon;
 using Autodesk.Windows;
 
+/// Prerequisites/dependencies:
+/// 
+///   RibbonEventManager: 
+///   IdleAction.cs: https://github.com/ActivistInvestor/AcMgdUtility/blob/main/IdleAction.cs
+///   
 /// This class provides the same functionality 
 /// founds in ExtensionApplicationAsync, along 
 /// with additional functionality for managing 
@@ -124,6 +129,11 @@ using Autodesk.Windows;
 /// startup and is never made visible for the life of the 
 /// AutoCAD session).
 /// 
+/// If your extension requires general, always-required
+/// initialization, override the Initialize() method in
+/// your derived class and do the needed initalization
+/// from within that override.
+/// 
 /// It is highly-recommended that ribbon context be created 
 /// only once and cached in memory, so that the same content 
 /// can be added to the ribbon again, each time this method 
@@ -142,12 +152,17 @@ namespace Autodesk.AutoCAD.Runtime.AIUtils
 {
    public abstract class RibbonExtensionApplication : IExtensionApplication
    {
+      protected object RibbonContent { get; private set; }
+      bool observingWorkspaceLoaded = false;
 
       /// <summary>
       /// optionally override this method to perform general
       /// initialization tasks that can have no dependence
-      /// on the ribbon, which may not yet exist when this
-      /// method is called.
+      /// on the ribbon (which may not yet exist when this
+      /// method is called). This method serves the same purpose
+      /// as the same-named method of ExtensionApplicationAsync.
+      /// This method is always called once when an extension is
+      /// loaded.
       /// </summary>
       
       protected virtual void Initialize()
@@ -155,18 +170,116 @@ namespace Autodesk.AutoCAD.Runtime.AIUtils
       }
 
       /// <summary>
-      /// Must be overridden in a derived type. This method 
+      /// Because it is necessary to repeatedly add the
+      /// same content to the ribbon (because loading a
+      /// workspace wipes out previously-added content),
+      /// The content should be created only once, and
+      /// stored in a variable, so that it can be added
+      /// whenever needed.
+      /// 
+      /// These methods will be called by this class to 
+      /// get the ribbon content. If an override returns
+      /// content, the override of AddContentToRibbon()
+      /// is called to add the content to the ribbon.
+      /// 
+      /// Derived types don't need to cache ribbon content,
+      /// because that is done by this class. Derived types
+      /// only need to create the content, and add it to the 
+      /// ribbon.
+      /// 
+      /// See the two overloads below for how this class
+      /// provides ways for derived types to do that.
+
+      /// <summary>
+      /// Override this method in a derived type to create 
+      /// and return the content that will be added to the 
+      /// ribbon whenever necessary, there's no need to cache
+      /// the created content manually, as this class does
+      /// that internally. Do not add the content to the
+      /// ribbon from this method. To do that, you override 
+      /// the AddRibbonContent() method and add the content
+      /// to the ribbon in the override of that method.
+      /// 
+      /// The purpose of this complexity, is to establish a
+      /// clear separation between the task of creating the 
+      /// content, and the task of adding the content to the 
+      /// ribbon, because the former must be done only once
+      /// and the latter must be done many times, and we do
+      /// not want be needlessly-burdened with creating the
+      /// same content many times over.
+      /// 
+      /// This method will be called only once per AutoCAD
+      /// session (unless InvalidateRibbonContent() is called,
+      /// to force it to be recreated, which exists mainly for
+      /// special use cases).
+      /// </summary>
+      /// <param name="state"></param>
+      /// <returns>The content to be added to the ribbon</returns>
+
+      protected virtual object CreateRibbonContent(RibbonState state)
+      {
+         return null;
+      }
+
+      /// <summary>
+      /// Override this method in a derived type to add 
+      /// content to the ribbon. This method can be called 
+      /// numerous times, so overrides should do nothing 
+      /// but add the content to the ribbon.
+      /// </summary>
+      /// <param name="ribbon">The RibbonControl</param>
+      /// <param name="content">The content to be added to the ribbon</param>
+      /// <param name="state">The current RibbonState</param>
+
+      protected virtual void AddContentToRibbon(RibbonControl ribbon, object content, RibbonState state)
+      {
+      }
+
+      /// <summary>
+      /// Clears/invalidates any cached ribbon content,
+      /// forcing it to be recreated.
+      /// </summary>
+
+      protected void InvalidateRibbonContent()
+      {
+         RibbonContent = null;
+      }
+
+      /// <summary>
+      /// Can be overridden in a derived type. This method 
       /// is called at the point where an existing ribbon 
       /// is found, or at some later point when the ribbon
       /// is created (which may never happen), this method
       /// will also be called (possibly many times) when a
       /// workspace is loaded.
+      /// 
+      /// The context parameter indicates the circumstances
+      /// under which the method is being called, but is
+      /// required only in special use-cases.
+      /// 
+      /// Generally, the context parameter should be ignored 
+      /// or not be involved in any type of branching because 
+      /// regardless of its value, the ribbon must be uniformly 
+      /// initialized every time this method is called.
+      /// 
+      /// If the CreateRibbonContent() and AddContentToRibbon()
+      /// methods are overridden, this method does not have to
+      /// be (and probably shouldn't be) overridden. 
       /// </summary>
       /// <param name="context">A value indicating the context
       /// in which the method is called.</param>
       /// <param name="ribbon">The RibbonControl</param>
 
-      protected abstract void InitializeRibbon(RibbonControl ribbon, RibbonState context);
+      protected virtual void InitializeRibbon(RibbonControl ribbon, RibbonState context)
+      {
+      }
+
+      void InitializeContent(RibbonControl ribbon, RibbonState context)
+      {
+         RibbonContent = RibbonContent ?? CreateRibbonContent(context);
+         if(RibbonContent != null)
+            AddContentToRibbon(RibbonControl, RibbonContent, context);
+      }
 
       /// <summary>
       /// Optionally, override this to perform cleanup
@@ -201,11 +314,8 @@ namespace Autodesk.AutoCAD.Runtime.AIUtils
             Application.Idle -= OnIdle;
             try
             {
-               this.Initialize();
-               if(RibbonPaletteSet != null)
-                  initializeRibbon(RibbonState.Active);
-               else
-                  RibbonServices.RibbonPaletteSetCreated += ribbonCreated;
+               Initialize();
+               InitializeRibbonCore(RibbonState.Active);
             }
             catch(System.Exception ex)
             {
@@ -215,29 +325,67 @@ namespace Autodesk.AutoCAD.Runtime.AIUtils
          }
       }
 
+      static void ExecuteInApplicationContext(Action action)
+      {
+         if(Application.DocumentManager.IsApplicationContext)
+            action();
+         else
+            IdleAction.OnIdle(action);
+      }
+
+      /// <summary>
+      /// Override to return the IRibbonContent provider whose
+      /// methods will be called to create and add content to
+      /// the ribbon. Derived types can implement this interface
+      /// and simply return 'this', or obtain an instance through
+      /// some other means.
+      /// </summary>
+      /// <param name="context"></param>
+      /// <returns></returns>
+
       void IExtensionApplication.Terminate()
       {
          this.Terminate();
       }
 
-      void initializeRibbon(RibbonState context)
+      void InitializeRibbonCore(RibbonState context)
       {
-         if(RibbonPaletteSet == null || RibbonControl == null)
-            throw new InvalidOperationException("Ribbon does not exist");
-         InitializeRibbon(RibbonControl, context);
-         RibbonPaletteSet.WorkspaceLoaded += workspaceLoaded;
+         if(RibbonPaletteSet != null)
+            ExecuteInApplicationContext(() => InitializeRibbonAsync(context));
+         else
+            RibbonServices.RibbonPaletteSetCreated += ribbonCreated;
       }
+
+      void InitializeRibbonAsync(RibbonState context)
+      {
+         InitializeRibbon(RibbonControl, context);
+         InitializeContent(RibbonControl, context);
+         AddWorkspaceLoadedHandler();
+      }
+
+      void AddWorkspaceLoadedHandler()
+      {
+         if(!observingWorkspaceLoaded)
+         {
+            RibbonPaletteSet.WorkspaceLoaded += workspaceLoaded;
+            observingWorkspaceLoaded = true;
+         }
+      }
+
+      /// <summary>
+      /// Event handlers
+      /// </summary>
 
       void ribbonCreated(object sender, EventArgs e)
       {
          RibbonServices.RibbonPaletteSetCreated -= ribbonCreated;
-         initializeRibbon(RibbonState.Initalizing);
+         InitializeRibbonCore(RibbonState.Initalizing);
       }
 
       void workspaceLoaded(object sender, EventArgs e)
       {
          if(Document != null)
-            InitializeRibbon(RibbonControl, RibbonState.WorkspaceLoaded);
+            InitializeRibbonCore(RibbonState.WorkspaceLoaded);
       }
 
       /// <summary>
@@ -253,7 +401,7 @@ namespace Autodesk.AutoCAD.Runtime.AIUtils
          RibbonServices.RibbonPaletteSet;
 
       protected static RibbonControl RibbonControl => 
-         RibbonPaletteSet?.RibbonControl;
+         RibbonPaletteSet.RibbonControl;
 
    }
 
@@ -282,4 +430,5 @@ namespace Autodesk.AutoCAD.Runtime.AIUtils
       /// </summary>
       WorkspaceLoaded = 2
    }
+
 }
