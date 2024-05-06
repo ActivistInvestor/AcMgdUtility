c3492765f6e4c680952489e38457c671f16f98e7
Tony <caddzooks@gmail.com>
2024-05-06 13:36:32 -0400

    Add files via upload

--- RibbonExtensionApplication/RibbonEventManager.cs
+++ RibbonExtensionApplication/RibbonEventManager.cs
@@ -249,54 +249,54 @@ namespace Autodesk.AutoCAD.ApplicationServices.AIUtils
             new IdleAction(action, quiescent, document);
          }
       }
+   }
+
+   /// <summary>
+   /// Indicates the context in which the 
+   /// InitializeRibbon event is raised.
+   /// </summary>
+
+   public enum RibbonState
+   {
+      /// <summary>
+      /// The ribbon exists but was not 
+      /// previously-initialized.
+      /// </summary>
+      Active = 0,
 
       /// <summary>
-      /// Indicates the context in which the 
-      /// InitializeRibbon event is raised.
+      /// The ribbon was just created.
       /// </summary>
+      Initalizing = 1,
 
-      public enum RibbonState
-      {
-         /// <summary>
-         /// The ribbon exists but was not 
-         /// previously-initialized.
-         /// </summary>
-         Active = 0,
+      /// <summary>
+      /// The ribbon exists and was previously
+      /// initialized, and a workspace was just
+      /// loaded, requiring application-provided
+      /// ribbon content to be added again.
+      /// </summary>
+      WorkspaceLoaded = 2,
 
-         /// <summary>
-         /// The ribbon was just created.
-         /// </summary>
-         Initalizing = 1,
-
-         /// <summary>
-         /// The ribbon exists and was previously
-         /// initialized, and a workspace was just
-         /// loaded, requiring application-provided
-         /// ribbon content to be added again.
-         /// </summary>
-         WorkspaceLoaded = 2,
-
-         /// <summary>
-         /// Indicates that ribbon content should be
-         /// reloaded for unspecified reasons.
-         /// </summary>
-         RefreshContent = 3
-      }
-
-      public delegate void RibbonStateEventHandler(object sender, RibbonStateEventArgs e);
-
-      public class RibbonStateEventArgs : EventArgs
-      {
-         public RibbonStateEventArgs(RibbonState state)
-         {
-            this.State = state;
-         }
-         public RibbonState State { get; private set; }
-         public RibbonPaletteSet RibbonPaletteSet =>
-            RibbonServices.RibbonPaletteSet;
-         public RibbonControl RibbonControl =>
-            RibbonPaletteSet?.RibbonControl;
-      }
-
+      /// <summary>
+      /// Indicates that ribbon content should be
+      /// reloaded for unspecified reasons.
+      /// </summary>
+      RefreshContent = 3
    }
+
+   public delegate void RibbonStateEventHandler(object sender, RibbonStateEventArgs e);
+
+   public class RibbonStateEventArgs : EventArgs
+   {
+      public RibbonStateEventArgs(RibbonState state)
+      {
+         this.State = state;
+      }
+      public RibbonState State { get; private set; }
+      public RibbonPaletteSet RibbonPaletteSet =>
+         RibbonServices.RibbonPaletteSet;
+      public RibbonControl RibbonControl =>
+         RibbonPaletteSet?.RibbonControl;
+   }
+
 }
