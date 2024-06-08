bae034fec6796b3274bb8b205e90c6ec441a7f3a
Tony <caddzooks@gmail.com>
2024-06-08 09:17:19 -0400

    Revisions and bug fixes

---
+++ RibbonEventManager/RibbonEventManagerExample.cs
@@ -0,0 +1,64 @@
+﻿
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices.AIUtils;
+using Autodesk.Windows;
+using Autodesk.AutoCAD.ApplicationServices;
+
+
+/// RibbonEventManagerExample.cs
+/// ActivistInvestor / Tony T
+/// 
+/// An example showing the use of the 
+/// RibbonEventManager class.
+/// 
+/// Source:
+///   
+///   https://github.com/ActivistInvestor/AcMgdUtility/blob/main/RibbonExtensionApplication/Examples/RibbonEventManagerExample.cs
+///
+
+/// TODO: Modify argument to be the name of 
+/// the actual IExtensionApplication-based class:
+
+[assembly: ExtensionApplication(typeof(Namespace1.MyApplication))]
+
+namespace Namespace1
+{
+   public class MyApplication : IExtensionApplication
+   {
+      static RibbonTab myRibbonTab;
+
+      public void Initialize()
+      {
+         /// Add a handler to the InitializeRibbon event.
+
+         RibbonEventManager.InitializeRibbon += initializeRibbon;
+      }
+
+      /// <summary>
+      /// Handler for the InitializeRibbon event:
+      /// </summary>
+
+      private void initializeRibbon(object sender, RibbonStateEventArgs e)
+      {
+         /// Create a ribbon tab on the 
+         /// first call to this method:
+
+         if(myRibbonTab == null)
+         {
+            myRibbonTab = new RibbonTab();
+            myRibbonTab.Id = "IDMyTab001";
+            myRibbonTab.Name = "MyRibbonTab";
+            myRibbonTab.Title = "MyRibbonTab";
+         }
+         
+         /// Add the tab to the ribbon:
+         
+         e.RibbonControl.Tabs.Add(myRibbonTab);
+      }
+
+      public void Terminate()
+      {
+      }
+   }
+
+}
