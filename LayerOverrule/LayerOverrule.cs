313136d45baacde9deec775b65c8efc80814eba5
Tony T <caddzooks@gmail.com>
2025-02-03 19:48:25 -0500

    Initial Commit

---
+++ LayerOverrule/LayerOverrule.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Reflection;
+using AcMgdLib.Overrules.Examples;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Internal;
+using Autodesk.AutoCAD.LayerManager;
+using Autodesk.AutoCAD.Runtime;
+
+namespace AcMgdLib.Overrules.Examples
+{
+
+   /// <summary>
+   /// Implementing the above example in a managed extension
+   /// requires it to be initialized in the Initializ7e() method
+   /// of an IExtensionApplication. The containing assembly
+   /// must also have the assembly:ExtensionApplication()
+   /// attribute to tell the runtime that the class should
+   /// initialized when the assembly is loaded. 
+   /// 
+   /// If there is an existing IExtensionApplication, the call 
+   /// to the Singleton<T>Initialize() method below can be placed 
+   /// in the existing IExtensionApplication's Initlialize() 
+   /// method, and this class can be omitted.
+   /// </summary>
+
+   ///  public class MyApplication : IExtensionApplication
+   ///  {
+   ///     public void Initialize()
+   ///     {
+   ///        SingletonManager<MyNoLockLayerOverrule>.Initlialize();
+   ///     }
+   ///  
+   ///     public void Terminate()
+   ///     {
+   ///     }
+   ///  }
+}
+
+namespace AcMgdLib.Overrules
+{
+   /// <summary>
+   /// The following abstract base class is designed to be 
+   /// resuable and not application-specific. It can be used 
+   /// to deal with modified layers by deriving a class from
+   /// it, and implementing application-specific code logic
+   /// in an override of the OnLayerModified() method, as is
+   /// done in the above example.
+   /// </summary>
+
+   public abstract class LayerOverrule : ObjectOverrule
+   {
+      string namePattern = "*";
+      static RXClass rxclass = RXObject.GetClass(typeof(LayerTableRecord));
+
+      public LayerOverrule(string namePattern = "*")
+      {
+         this.namePattern = namePattern ?? "*";
+         AddOverrule(rxclass, this, true);
+      }
+
+      public override void Close(DBObject obj)
+      {
+         if(obj.IsWriteEnabled &&
+               obj.IsModified &&
+               !obj.IsUndoing &&
+               obj.IsReallyClosing &&
+               obj is LayerTableRecord layer && IsMatch(layer))
+            OnLayerModified(layer);
+         base.Close(obj);
+      }
+
+      protected virtual bool IsMatch(LayerTableRecord layer)
+      {
+         bool result = namePattern == "*" || Utils.WcMatchEx(layer.Name, namePattern, true);
+         return result;
+      }
+
+      protected abstract void OnLayerModified(LayerTableRecord layer);
+
+      protected override void Dispose(bool disposing)
+      {
+         if(!base.IsDisposed)
+            RemoveOverrule(rxclass, this);
+         base.Dispose(disposing);
+      }
+   }
+}
+
+/// Utility and helper classes used by the above code:
+namespace AcMdgLib.Common
+{
+   /// <summary>
+   /// Helper class to invoke actions asynchronously
+   /// on next Application.Idle event:
+   /// </summary>
+
+   public class AsyncHelper
+   {
+      private Action action;
+
+      AsyncHelper(Action action)
+      {
+         this.action = action;
+         Application.Idle += idle;
+      }
+
+      void idle(object sender, EventArgs e)
+      {
+         Application.Idle -= idle;
+         if(action != null)
+            action();
+         action = null;
+      }
+
+      public static void WriteLine(string msg, params object[] args)
+      {
+         OnIdle(() => WriteMsg(msg, args));
+      }
+
+      static void WriteMsg(string msg, params object[] args)
+      {
+         Document doc = Application.DocumentManager.MdiActiveDocument;
+         if(doc != null)
+            doc.Editor.WriteMessage("\n" + msg, args);
+      }
+
+      public static void OnIdle(Action action)
+      {
+         new AsyncHelper(action);
+      }
+   }
+
+   /// <summary>
+   /// Helper class to manage singleton instances of
+   /// of types that implement IDisposable. The managed
+   /// type must have a public default constructor.
+   /// </summary>
+   /// <typeparam name="T">The type to be managed</typeparam>
+
+   public static class DisposableSingleton<T> where T : class, IDisposable, new()
+   {
+      static T instance = null;
+
+      public static bool Enabled
+      {
+         get => instance != null;
+         set
+         {
+            if(instance != null ^ value)
+            {
+               if(value)
+                  instance = new T();
+               else
+               {
+                  instance?.Dispose();
+                  instance = null;
+               }
+            }
+         }
+      }
+
+      public static T Instance => instance;
+   }
+
+   /// <summary>
+   /// A class that backdoors the LayerManager to 
+   /// force it to Update.
+   /// </summary>
+
+   public static class LayerManagerThunk
+   {
+      static LayerManagerControl layerManagerControl;
+
+      public static void UpdateLayerManager(bool async = true)
+      {
+         if(Initialize() && layerManagerControl.Visible &&
+            !layerManagerControl.LayerViewManager.Modal)
+         {
+            if(!async)
+               layerManagerControl.UpdateLayerManager(true);
+            else
+               AsyncHelper.OnIdle(() => layerManagerControl.UpdateLayerManager(true));
+         }
+      }
+
+      static bool Initialize()
+      {
+         if(layerManagerControl == null)
+         {
+            FieldInfo field = typeof(PaletteHost).GetField("layerManager_",
+               BindingFlags.Static | BindingFlags.NonPublic);
+            object rslt = field.GetValue(null);
+            if(rslt is LayerManagerControl lmc)
+               layerManagerControl = lmc;
+         }
+         return layerManagerControl != null;
+      }
+   }
+
+}
+
---
+++ LayerOverrule/LayerOverruleExample.cs
@@ -0,0 +1,103 @@
+﻿using AcMdgLib.Common;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+
+namespace AcMgdLib.Overrules.Examples
+{
+   /// <summary>
+   /// Example showing how to use the LayerOverrule
+   /// base type (included below).
+   /// 
+   /// This example prevents layers from being locked
+   /// only if their name starts with "NOLOCK", which
+   /// is just an example. Real-world application of
+   /// this class can involve realistic criteria.
+   /// 
+   /// Issue:
+   /// 
+   /// The Layer properties manager does not update to
+   /// display the correct locked/unlocked state of a
+   /// layer which the user tried to lock via the layer
+   /// manager dialog. 
+   /// 
+   /// So, if the user tries to lock a layer that this
+   /// overrule does not allow them to lock, the layer
+   /// properties manager will show the layer as being
+   /// locked, even thought it is not locked.
+   /// 
+   /// The LayerManagerThunk class included in the code
+   /// file required by this example addresses that issue,
+   /// by forcing the LayerManager control to update when
+   /// the user tries to lock a layer that can't be locked.
+   /// 
+   /// </summary>
+
+   public class MyNoLockLayerOverrule : LayerOverrule
+   {
+      public MyNoLockLayerOverrule() : base("NOLOCK*")
+      {
+      }
+
+      /// <summary>
+      /// The base type filters the layers by their names
+      /// according to the string passed to its constructor
+      /// above, so this method will only be called if the 
+      /// layer's name matches the wildcard pattern "NOLOCK*".
+      /// 
+      /// You can change the pattern to meet your needs or
+      /// you can override the IsMatch() method to directly
+      /// specify what layers the overrule should act on.
+      /// </summary>
+
+      protected override void OnLayerModified(LayerTableRecord layer)
+      {
+         if(layer.IsLocked)
+         {
+            layer.IsLocked = false;
+            LayerManagerThunk.UpdateLayerManager(true);
+            AsyncHelper.WriteLine($"\nALERT: Layer {layer.Name} cannot be locked!");
+         }
+      }
+   }
+
+   /// <summary>
+   /// Implementing the above example in a managed extension
+   /// requires it to be initialized in the Initializ7e() method
+   /// of an IExtensionApplication. The containing assembly
+   /// must also have the assembly:ExtensionApplication()
+   /// attribute to tell the runtime that the class should
+   /// initialized when the assembly is loaded. 
+   /// 
+   /// If there is an existing IExtensionApplication, the call 
+   /// to the Singleton<T>Initialize() method below can be placed 
+   /// in the existing IExtensionApplication's Initlialize() 
+   /// method, and this class can be omitted.
+   /// </summary>
+
+   ///  public class MyApplication : IExtensionApplication
+   ///  {
+   ///     public void Initialize()
+   ///     {
+   ///        SingletonManager<MyNoLockLayerOverrule>.Initlialize();
+   ///     }
+   ///  
+   ///     public void Terminate()
+   ///     {
+   ///     }
+   ///  }
+
+   /// A test class that can be used to toggle
+   /// the example overrule using a command:
+
+   public static class NoLockLayersCommand
+   {
+      [CommandMethod("NOLOCKLAYERS")]
+      public static void NoLockLayers()
+      {
+         DisposableSingleton<MyNoLockLayerOverrule>.Enabled ^= true;
+      }
+   }
+
+
+}
+
